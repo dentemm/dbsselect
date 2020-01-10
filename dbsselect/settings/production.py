47b50e096bdeff3890c900a8fc4e026278ee754a
Tim Claes <tim.claes@me.com>
2020-01-10 12:17:25 +0100

    updated production settings

--- dbsselect/settings/production.py
+++ dbsselect/settings/production.py
@@ -6,3 +6,37 @@ try:
     from .local import *
 except ImportError:
     pass
+
+
+WSGI_APPLICATION = 'uniekgroen.wsgi.application'
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.dbsselect.be', '.dbsselect.com', '.aws.amazon.com', '.herokuapp.com']
+
+
+#
+# WHITENOISE CONFIG
+#
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#
+# DATABASE
+#
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# AMAZON AWZS
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_STORAGE_BUCKET_NAME = 'dbsselect'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.eu-central-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
