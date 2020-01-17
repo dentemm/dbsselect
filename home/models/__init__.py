f2a2c156505fd3c4cc85fa002d238e530054c488
Tim Claes <tim.claes@me.com>
2020-01-17 14:39:25 +0100

    modelling data

--- home/models/__init__.py
+++ home/models/__init__.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 from wagtail.core.models import Page, Orderable
-from wagtail.admin.edit_handlers import InlinePanel, MultiFieldPanel
+from wagtail.admin.edit_handlers import InlinePanel, MultiFieldPanel, FieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
-# from wagtail.core.fields import StreamField, RichTextField
+from wagtail.core.fields import RichTextField
 
 from modelcluster.fields import ParentalKey
 from modelcluster.contrib.taggit import ClusterTaggableManager
@@ -17,21 +17,75 @@ class HomePagePartner(Orderable, Partner):
     class Meta:
         ordering = ['sort_order']
 
-class HomePageTestimonial(Orderable, Testimonial):
+# class HomePageTestimonial(Orderable, Testimonial):
 
-    page = ParentalKey('home.HomePage', on_delete=models.CASCADE, related_name='testimonials')
+#     page = ParentalKey('home.HomePage', on_delete=models.CASCADE, related_name='testimonials')
 
-    class Meta:
-        ordering = ['sort_order']
+
+#     class Meta:
+#         ordering = ['sort_order']
 
 
 class HomePage(Page):
 
-    # content = StreamField(HomePageStreamBlock(), null=True)
-    # partners = ClusterTaggableManager(through=HomePagePartner, blank=True)
-    pass
+    subtitle = RichTextField('Ondertitle', null=True, features=['bold', ])
 
-HomePage.content_panels = Page.content_panels + [
+    # SECTION 1
+    what = RichTextField('Wat?', null=True, features=['bold', ])
+    what_info = RichTextField('Info', null=True, features=['bold', 'link', 'hr'])
+    what_image = models.ForeignKey(
+        'wagtailimages.Image',
+        verbose_name='Afbeelding',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        null=True,
+    )
+    download_text = models.CharField('Download tekst', max_length=32, default='Download DBS Brochure')
+
+    sessions = RichTextField('Sessies', null=True, features=['bold', ])
+    sessions_info = RichTextField('Info', null=True, features=['bold', 'link', 'hr'])
+    sessions_image = models.ForeignKey(
+        'wagtailimages.Image',
+        verbose_name='Afbeelding',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        null=True,
+    )
+    data_text = models.CharField('Data tekst', max_length=32, default='Check dates') 
+
+    # RichTextField('Beschrijving', blank=True, null=True, features=['h5', 'h6', 'bold', 'italic', 'link', 'hr', 'blockquote'])
+
+    # content = StreamField(HomePageStreamBlock(), null=True)
+
+
+HomePage.content_panels = [
+    MultiFieldPanel(
+        [
+            FieldPanel('title', classname='col8'),
+            FieldPanel('subtitle', classname='col8')
+        ],
+        heading='Algemene informatie'
+    ),
+    MultiFieldPanel(
+        [
+            FieldPanel('what', classname='col8'),
+            FieldPanel('what_info', classname='col8'),
+            FieldPanel('download_text', classname='col8'),
+            ImageChooserPanel('what_image', classname='col8')
+        ],
+        heading='Sectie 1: wat is BDS?',
+        classname='collapsible collapsed'
+    ),
+    MultiFieldPanel(
+        [
+            FieldPanel('sessions', classname='col8'),
+            FieldPanel('sessions_info', classname='col8'),
+            FieldPanel('data_text', classname='col8'),
+            ImageChooserPanel('sessions_image', classname='col8')
+        ],
+        heading='Sectie 1: BDS Select sessies',
+        classname='collapsible collapsed'
+    ), 
     MultiFieldPanel(
         [
             InlinePanel('partners')
