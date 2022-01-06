747512cdc1687372cc1c4c2b3b7090cd726e0ce0
Tim Claes <tim.claes@me.com>
2022-01-06 21:36:49 +0100

    upgrading wagtail

--- dbsselect/settings/production.py
+++ dbsselect/settings/production.py
@@ -31,7 +31,6 @@ import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 # AMAZON AWZS
-
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_STORAGE_BUCKET_NAME = 'dbsselect'
