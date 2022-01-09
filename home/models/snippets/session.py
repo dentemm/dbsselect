320739fca49edcc8991517490a414ad8a63d0860
Tim Claes <tim.claes@me.com>
2022-01-09 19:09:35 +0100

    chore: improvements to sessions

--- home/models/snippets/session.py
+++ home/models/snippets/session.py
@@ -6,6 +6,13 @@ from wagtail.snippets.models import register_snippet
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
+LANGUAGE_CHOICES = (
+    ('EN', 'English'),
+    ('NL', 'Nederlands'),
+    ('FR', 'Francais'),
+    ('DE', 'Deutsch'),
+)
+
 @register_snippet
 class Session(models.Model):
 
@@ -17,9 +24,10 @@ class Session(models.Model):
     related_name='+',
     null=True
   )
+  language = models.CharField(choices=LANGUAGE_CHOICES, default='EN', null=True, max_length=18)
 
   def __str__(self):
-    return f'Sessie {self.date.strftime("%d %m")}'
+    return f'Sessie {self.date.strftime("%d %m")} ({self.language})'
 
   class Meta:
     verbose_name = 'Sessie'
@@ -30,6 +38,7 @@ Session.panels = [
   MultiFieldPanel([
     FieldPanel('date', classname='col8'),
     FieldPanel('link', classname='col8'),
+    FieldPanel('language', classname='col8'),
     ImageChooserPanel('image', classname='col10'),
 	], 
     heading='Persoonsgegevens'
