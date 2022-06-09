b593b189593d374f875714fb8bcbe4af1c8378f3
Tim Claes <tim.claes@me.com>
2022-06-09 14:47:09 +0200

    chore: updating navigation

--- home/templatetags/custom_tags.py
+++ home/templatetags/custom_tags.py
@@ -6,28 +6,23 @@ register = template.Library()
 
 @register.simple_tag
 def sessions_page(locale):
-  return SessionsPage.objects.all().first().get_url()
-
-  # return SessionsPage.objects.all().filter(locale=locale).first().get_url()
+  return SessionsPage.objects.all().filter(locale=locale).first().get_url()
 
 @register.simple_tag
 def contact_page(locale):
-  return ContactPage.objects.all().first().get_url()
-
-  # return ContactPage.objects.all().filter(locale=locale).first().get_url()
+  return ContactPage.objects.all().filter(locale=locale).first().get_url()
 
 @register.simple_tag
 def about_page(locale):
-  return AboutPage.objects.all().first().get_url()
-  # return AboutPage.objects.all().filter(locale=locale).first().get_url()
+  return AboutPage.objects.all().filter(locale=locale).first().get_url()
 
 @register.simple_tag
 def media_page(locale):
-  return MediaPage.objects.all().first().get_url()
+  return MediaPage.objects.all().filter(locale=locale).first().get_url()
 
 @register.simple_tag
 def relive_page(locale):
-  return RelivePage.objects.all().first().get_url()
+  return RelivePage.objects.all().filter(locale=locale).first().get_url()
 
 @register.simple_tag
 def check_current_page(slug, reference):
